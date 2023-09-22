b7801d3f3b36e1789e19c154bd09d8a65b1031b4
Ira Siddiqui <120197629+irasiddiqui@users.noreply.github.com>
2023-09-22 14:31:35 +0530

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+let firebaseConfig = {
+    //firebase credentials
+    apiKey: "AIzaSyBR1QrFYUYPtpL329yCCoNkizzWyIc5B-s",
+    authDomain: "curiousquill-85f94.firebaseapp.com",
+    projectId: "curiousquill-85f94",
+    storageBucket: "curiousquill-85f94.appspot.com",
+    messagingSenderId: "797784092209",
+    appId: "1:797784092209:web:489844c851f49b7a172f10"
+};
+
+// firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+// let db = firebase.firestore();
